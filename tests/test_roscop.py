fb8a8a2fb336fdfeb4a31d93bd2ca506e6d84093
jacques Grelet <jacques.grelet@gmail.com>
2019-03-13 23:59:12 +0000

    add test for PSAL

--- tests/test_roscop.py
+++ tests/test_roscop.py
@@ -24,10 +24,23 @@ class RoscopTest(unittest.TestCase):
         ''' test the number of entries in csv file '''
         self.assertEqual(len(self.r), 68)
 
-    def test_key(self):
+    def test_key_TEMP(self):
         ''' test the standard_name for physical parameter TEMP '''
         self.assertEqual(self.r.returnCode('TEMP')[
                          'standard_name'], 'sea_water_temperature')
+        self.assertEqual(self.r.returnCode('TEMP')[
+                         'units'], 'degree_Celsius')
+        self.assertEqual(self.r.returnCode('TEMP')[
+                         'comment'], 'Ocean temperature in Degrees Celsius')
+
+    def test_key_PSAL(self):
+        ''' test the standard_name for physical parameter PSAL '''
+        self.assertEqual(self.r.returnCode('PSAL')[
+                         'standard_name'], 'sea_water_salinity')
+        self.assertEqual(self.r.returnCode('PSAL')[
+                         'units'], '1')
+        self.assertEqual(self.r.returnCode('PSAL')[
+                         'comment'], 'Ocean practical salinity (PSS-78)')
 
 
 if __name__ == '__main__':
