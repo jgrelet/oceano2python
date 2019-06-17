8dc6f622d6be8ea3572b36f9eb4156e940af51d7
jacques Grelet <Jacques.Grelet@ird.fr>
2019-06-17 11:55:13 +0200

    add ctd.header section

--- config.py
+++ config.py
@@ -8,28 +8,41 @@ print(cfg['global']['author'])
 
 d = cfg['global']
 print(d, end='\n')
+print(end='\n')
 
 d = cfg['cruise']
 print(d, end='\n')
+print(end='\n')
 
 d = cfg['ctd']
 print(d, end='\n')
+print(end='\n')
+
+d = cfg['ctd']['header']
+print(d, end='\n')
+for key in d.keys():
+    print("{}: {}".format(key, d[key]))
+print(end='\n')
 
 for key in cfg['cruise'].keys():
     print(cfg['cruise'][key])
     # print(cfg['cruise'].get(key))
+print(end='\n')
 
 d = cfg['split']['ctd']
 print(d, end='\n')
 for key in d.keys():
     print("{}: {}".format(key, d[key]))
+print(end='\n')
 
 d = cfg['split']['ctdAll']
 print(d, end='\n')
 for key in d.keys():
     print("{}: {}".format(key, d[key]))
+print(end='\n')
 
 d = cfg['split']['btl']
 print(d, end='\n')
 for key in d.keys():
     print("{}: {}".format(key, d[key]))
+print(end='\n')
