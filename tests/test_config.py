58727b38e05d7f5f4768c35f5357fb7f70eaf9e9
Jacques Grelet <jacques.grelet@gmail.cpm>
2022-04-21 11:30:44 -0300

    update tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -39,13 +39,16 @@ class testConfig(unittest.TestCase):
                        'creator': 'Jacques.Grelet@ird.fr'}
 
         self.ctd = {'cruisePrefix': 'fr29',
-                    'station': 'Station\s*:\s*\D*(\d*)',
                     'stationPrefixLength': 3,
                     'titleSummary': 'CTD profiles processed during PIRATA-FR29 cruise',
                     'typeInstrument': 'SBE911+',
-                    'instrumentNumber': '09P1263'}
+                    'instrumentNumber': '09P1263',
+                    'dateTimeFormat': "%d/%b/%Y %H:%M:%S",
+                    'julianOrigin': 1}
 
-        self.ctdHeader = {'isHeader': '^[*#]',
+        self.ctdHeader = {'isHeader': '^[*#]+\s+',
+                          'endHeader': '^[*]END[*]',
+                          'station': 'Station\s*:\s*\D*(\d*)',
                           'LATITUDE':  'NMEA\s+Latitude\s*[:=]\s*(\d+)\s+(\d+\.\d+)\s+(\w)',
                           'LONGITUDE': 'NMEA\s+Longitude\s*[:=]\s*(\d+)\s+(\d+.\d+)\s+(\w)',
                           'DATETIME': 'System UpLoad Time\s*=\s*(\w+)\s+(\d+)\s+(\d+)\s+(\d+):(\d+):(\d+)',
@@ -65,9 +68,11 @@ class testConfig(unittest.TestCase):
                     'type': "SIPPICAN",
                     'sn': "01150",
                     'title_summary': "XBT profiles processed during PIRATA-FR29 cruise",
-                    'comment': "Extract from .edf files"}
+                    'comment': "Extract from .edf files",
+                    'dateTimeFormat': "%d/%m/%Y %H:%M:%S"}
 
-        self.xbtHeader = {'endHeader': 'Depth\s*\(m\)',
+        self.xbtHeader = {'endHeader': '^Depth\s*\(m\)',
+                          'station': 'Sequence\s*#\s*:\s*(\d*)',
                           'TIME': 'Time of Launch\s*[:=]\s*(\d+):(\d+):(\d+)',
                           'DATE': 'Date of Launch\s*[:=]\s*(\d+)/(\d+)/(\d+)',
                           'DATETIME': 'System UpLoad Time\s*=\s*(\w+)\s+(\d+)\s+(\d+)\s+(\d+):(\d+):(\d+)',
@@ -80,10 +85,11 @@ class testConfig(unittest.TestCase):
         self.splitCtdAll = {'ETDD': 1, 'PRES': 2, 'DEPTH': 3, 'TE01': 4, 'TE02': 5, 'CND1': 6, 'CND2': 7, 'DOV1': 8, 'DOV2': 9, 'DVT1': 10, 'DVT2': 11,
                             'TUR3': 12, 'FLU2': 13, 'FLU3': 14, 'DO12': 15, 'DO22': 16, 'PSA1': 17, 'PSA2': 18, 'DEN1': 19, 'DEN2': 20, 'SVEL': 21, 'NAVG': 23}
 
-        self.splitBtl = {'BOTL': 1, 'month': 2, 'day': 3, 'year': 4, 'PSA1': 5, 'PSA2': 6, 'DO11': 7,
-                         'DO12': 8, 'DO21': 9, 'DO22': 10, 'Potemp090C': 11, 'Potemp190C': 12,
-                         'ETDD': 13, 'PRES': 14, 'DEPH': 15, 'TE01': 16, 'TE02': 17, 'CND1': 18, 'CND2': 19,
-                         'DOV1': 20, 'DOV2': 21, 'DVT1': 22, 'DVT2': 23, 'TUR3': 24, 'FLU2': 25}
+        self.splitBtl = {'BOTL': 0, 'PSA1': 4, 'PSA2': 5, 'DO11': 6,
+                         'DO12': 7, 'DO21': 8, 'DO22': 9, 'Potemp090C': 10, 'Potemp190C': 11,
+                         'ETDD': 12, 'PRES': 13, 'DEPTH': 14, 'TE01': 15, 'TE02': 16, 'CND1': 17, 
+                         'CND2': 18, 'DOV1': 19, 'DOV2': 20, 'DVT1': 21, 'DVT2': 22, 'TUR3': 23, 
+                         'FLU2': 24}
 
         self.splitXbt = {'DEPTH': 0, 'TEMP': 1, 'SVEL': 2}
 
